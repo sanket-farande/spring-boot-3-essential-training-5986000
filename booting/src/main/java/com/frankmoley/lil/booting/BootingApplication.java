f092574ea580242b5646b8588a931b41fd9891fe
Sanket F. <46294232+sanket-farande@users.noreply.github.com>
2025-12-24 10:05:30 +0000

    Completed booting web application

--- booting/src/main/java/com/frankmoley/lil/booting/BootingApplication.java
+++ booting/src/main/java/com/frankmoley/lil/booting/BootingApplication.java
@@ -3,6 +3,8 @@ package com.frankmoley.lil.booting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Configures this class as a Spring Boot application
+// Is equivalent to using @Configuration, @EnableAutoConfiguration, and @ComponentScan with their default attributes
 @SpringBootApplication
 public class BootingApplication {
 
